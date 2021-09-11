cad8c53e966c05e5c6be6ea104c8b218626c99dd
wvangaalen <88194699+wvangaalen@users.noreply.github.com>
2021-09-11 22:52:21 +0200

    Update LinkedLogo.yr6MvfaTaLS4.js

--- File1/dist/components/LinkedLogo.yr6MvfaTaLS4.js
+++ File1/dist/components/LinkedLogo.yr6MvfaTaLS4.js
@@ -1,15 +1,11 @@
 import {
-    h as e
+    h as C
 } from "../../_snowpack/pkg/preact.hVpyV6Atzm6w.js";
-import o from "./Heading.pbOTat_MC3pH.js";
-import t from "./Logo.htCzTMvRi1b3.js";
-export default function r() {
-    return e(o, {
-        size: "lg"
-    }, e("a", {
-        className: "transition-colors flex items-center space-x-4 dark:text-white hover:text-blue-500",
-        href: "/"
-    }, e("div", {
-        className: "w-10"
-    }, e(t, null)), "AI Solutions"))
+export default function o() {
+    return C("svg", {
+        viewBox: "0 0 512 512",
+        className: "fill-current"
+    }, C("path", {
+        d: ""
+    }))
 }
