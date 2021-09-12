31b3e896bf141afe45dbce3dea2504bd781ade99
wvangaalen <88194699+wvangaalen@users.noreply.github.com>
2021-09-12 12:45:17 +0200

    Add files via upload

---
+++ File1/src/Sidebar.jsx
@@ -0,0 +1,52 @@
+import { h, Fragment } from 'preact';
+import LinkedLogo from './components/LinkedLogo';
+import { Match } from 'preact-router/match';
+import { memo } from 'preact/compat';
+import { ENV } from './env';
+import { useConfig } from './api';
+import { useMemo } from 'preact/hooks';
+import NavigationDrawer, { Destination, Separator } from './components/NavigationDrawer';
+
+export default function Sidebar() {
+  const { data: config } = useConfig();
+  const cameras = useMemo(() => Object.keys(config.cameras), [config]);
+
+  return (
+    <NavigationDrawer header={<Header />}>
+      <Destination href="/" text="Cameras" />
+      <Match path="/cameras/:camera/:other?">
+        {({ matches }) =>
+          matches ? (
+            <Fragment>
+              <Separator />
+              {cameras.map((camera) => (
+                <Destination href={`/cameras/${camera}`} text={camera} />
+              ))}
+              <Separator />
+            </Fragment>
+          ) : null
+        }
+      </Match>
+      <Destination href="/events" text="Events" />
+      <Destination href="/debug" text="Debug" />
+      <Separator />
+      <div className="flex flex-grow" />
+      {ENV !== 'production' ? (
+        <Fragment>
+          <Destination href="/styleguide" text="Style Guide" />
+          <Separator />
+        </Fragment>
+      ) : null}
+      <Destination className="self-end" href="https://192.168.149.59/api/events" text="API" />
+      <Destination className="self-end" href="https://ai.ai-solutions.com/Contact" text="Contact" />
+    </NavigationDrawer>
+  );
+}
+
+const Header = memo(() => {
+  return (
+    <div className="text-gray-500">
+      <LinkedLogo />
+    </div>
+  );
+});
